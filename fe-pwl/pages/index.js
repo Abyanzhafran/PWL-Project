f004cee9e4a249ca6fa5552c0bd87b7fcd471422
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-12-04 12:30:56 +0700

    feat: update add eslint-config-prettier add .prettierrc.json file update package

--- fe-pwl/pages/index.js
+++ fe-pwl/pages/index.js
@@ -1,15 +1,15 @@
 const homepage = () => {
   return (
     <div className="flex gap-4 mt-20">
-    <h1 className="text-2xl font-bold text-gray-500">yoi mamen</h1>
-      <button class="btn">neutral</button> 
-      <button class="btn btn-primary">primary</button> 
-      <button class="btn btn-secondary">secondary</button> 
-      <button class="btn btn-accent">accent</button> 
-      <button class="btn btn-ghost">ghost</button> 
+      <h1 className="text-2xl font-bold text-gray-500">yoi mamen</h1>
+      <button class="btn">neutral</button>
+      <button class="btn btn-primary">primary</button>
+      <button class="btn btn-secondary">secondary</button>
+      <button class="btn btn-accent">accent</button>
+      <button class="btn btn-ghost">ghost</button>
       <button class="btn btn-link">link</button>
     </div>
-  )
-}
+  );
+};
 
-export default homepage
+export default homepage;

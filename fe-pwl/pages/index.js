4d7798941e69121a57e1b5b9c598a6afea601008
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-12-07 16:49:12 +0700

    feat: add experimental api fe

--- fe-pwl/pages/index.js
+++ fe-pwl/pages/index.js
@@ -1,15 +1,43 @@
-const homepage = () => {
+const Homepage = () => {
+  let itemTab = [
+    {
+      num: 1,
+      name: 'Joki awodi',
+      job: 'masak awkdj',
+    },
+  ];
+
   return (
-    <div className="flex gap-4 mt-20">
-      <h1 className="text-2xl font-bold text-gray-500">yoi mamen</h1>
-      <button class="btn">neutral</button>
-      <button class="btn btn-primary">primary</button>
-      <button class="btn btn-secondary">secondary</button>
-      <button class="btn btn-accent">accent</button>
-      <button class="btn btn-ghost">ghost</button>
-      <button class="btn btn-link">link</button>
+    <div className="flex items-center justify-center">
+      <div className="mt-10">
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {itemTab.map((item) => (
+                <tr>
+                  <th>{item.num}</th>
+                  <td>{item.name}</td>
+                  <td>{item.job}</td>
+                  <td className="flex gap-2">
+                    <button className="btn btn-success btn-sm">edit</button>
+                    <button className="btn btn-error btn-sm">delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default homepage;
+export default Homepage;

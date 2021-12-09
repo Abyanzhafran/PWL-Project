ff832b35e3109d2c16ed46dd659c36445a828898
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-12-09 09:52:49 +0700

    feat: add get api laravel

--- be-pwl/routes/api.php
+++ be-pwl/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApasiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::resource('Apasi', 'ApasiController')->except(['create', 'edit']);
+
+Route::get('/apasi', function () {
+    return 'mamen';
+});
+
+Route::get('/index', [ApasiController::class, 'index']);

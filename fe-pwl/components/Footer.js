a52f380c4bea1031b734c1e5aa24bd2c0be76157
datyukianesa <funkyfanni233@gmail.com>
2021-12-19 01:02:11 +0700

    added mui icon

---
+++ fe-pwl/components/Footer.js
@@ -0,0 +1,34 @@
+export default function Footer() {
+  return (
+    <footer className="footer p-10 bg-primary text-secondary">
+      <div>
+        <span className="footer-title text-lg">About Us</span>
+        <p>
+          Airgun Indonesia dibuka pada tahun 2017 oleh Travis Scott arya, Airgun
+          Indonesia bergerak dalam bidang jual beli senapan pcp, pelet, Air
+          Pitols, airguns, airsoft, paintball, blank gun, crossbows dan
+          perlengkapan berburu.
+        </p>
+      </div>
+      <div>
+        <span className="footer-title text-lg">Reach Us</span>
+        <p>+6288 7876 2321</p>
+        <div className="grid grid-flow-col gap-4">
+          <a href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              class="fill-current"
+            >
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+            </svg>
+          </a>
+          <a href="#"></a>
+          <a href="#"></a>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- fe-pwl/pages/index.js
+++ fe-pwl/pages/index.js
@@ -1,12 +1,17 @@
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import Layout from '../components/Layout';
 import Head from 'next/head';
 
 const homepage = () => {
   return (
     <>
+      <Head>
+        <title>Airgun Indonesia</title>
+      </Head>
       <Navbar></Navbar>
       <Layout></Layout>
+      <Footer></Footer>
     </>
   );
 };

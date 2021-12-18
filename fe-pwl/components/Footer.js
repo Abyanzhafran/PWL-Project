ea029b0f456baa07522420bc895ebd01da1e4219
datyukianesa <funkyfanni233@gmail.com>
2021-12-19 01:30:20 +0700

    footer comp

--- fe-pwl/components/Footer.js
+++ fe-pwl/components/Footer.js
@@ -1,32 +1,35 @@
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 export default function Footer() {
   return (
     <footer className="footer p-10 bg-primary text-secondary">
-      <div>
-        <span className="footer-title text-lg">About Us</span>
-        <p>
-          Airgun Indonesia dibuka pada tahun 2017 oleh Travis Scott arya, Airgun
-          Indonesia bergerak dalam bidang jual beli senapan pcp, pelet, Air
-          Pitols, airguns, airsoft, paintball, blank gun, crossbows dan
-          perlengkapan berburu.
-        </p>
-      </div>
-      <div>
-        <span className="footer-title text-lg">Reach Us</span>
-        <p>+6288 7876 2321</p>
-        <div className="grid grid-flow-col gap-4">
-          <a href="#">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              class="fill-current"
-            >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-            </svg>
-          </a>
-          <a href="#"></a>
-          <a href="#"></a>
+      <div className="grid grid-flow-col">
+        <div className="flex justify-around gap-10">
+          <div className="w-1/2 lg:w-1/4 text-lg">
+            <span className="footer-title">About Us</span>
+            <p className="text-justify">
+              Airgun Indonesia dibuka pada tahun 2017 oleh Travis Scott arya,
+              Airgun Indonesia bergerak dalam bidang jual beli senapan pcp,
+              pelet, Air Pitols, airguns, airsoft, paintball, blank gun,
+              crossbows dan perlengkapan berburu.
+            </p>
+          </div>
+          <div className="text-lg xl:text-xl">
+            <span className="footer-title">Reach Us</span>
+            <p>+6288 7876 2321</p>
+            <div className="grid grid-flow-col gap-4">
+              <a href="#">
+                <FacebookIcon></FacebookIcon>
+              </a>
+              <a href="#">
+                <InstagramIcon></InstagramIcon>
+              </a>
+              <a href="#">
+                <WhatsAppIcon></WhatsAppIcon>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </footer>

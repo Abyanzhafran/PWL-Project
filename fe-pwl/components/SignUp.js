ebd50a8140a800c5f70c94f032ef2fdbfb98413f
datyukianesa <funkyfanni233@gmail.com>
2021-12-23 02:49:33 +0700

    add : sign up page add : sign up svg

--- fe-pwl/components/SignUp.js
+++ fe-pwl/components/SignUp.js
@@ -1,3 +1,81 @@
-export default function () {
-  return <div>Enter</div>;
+export default function SignUp() {
+  return (
+    <div className="card card-bordered bg-white w-1/2 lg:w-1/3">
+      <div className="card-body">
+        <div className="card-title mb-5">
+          <h1 className="text-2xl font-bold uppercase">Create Account</h1>
+        </div>
+        <div className="flex-col">
+          <form action="">
+            <div className="form-control">
+              <label className="label">
+                <label htmlFor="Nama" className="label-text">
+                  Name
+                </label>
+              </label>
+              <input
+                type="text"
+                name="txtNama"
+                id="txtNama"
+                className="input input-ghost shadow-md"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <label htmlFor="Alamat" className="label-text">
+                  Alamat
+                </label>
+              </label>
+              <textarea
+                name="txtAlamat"
+                id="txtAlamat"
+                cols="30"
+                rows="10"
+                className="textarea h-24 textarea-ghost shadow-lg"
+              ></textarea>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <label htmlFor="Username" className="label-text">
+                  Username
+                </label>
+              </label>
+              <input
+                type="text"
+                name="txtUsername"
+                id="txtUsername"
+                className="input input-ghost shadow-md"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <label htmlFor="Password" className="label-text">
+                  Password
+                </label>
+              </label>
+              <input
+                type="password"
+                name="txtPassword"
+                id="txtPassword"
+                className="input input-ghost shadow-md"
+              />
+            </div>
+            <div className="form-control mt-5">
+              <label htmlFor="" className="label">
+                <label htmlFor="" className="label-text-alt">
+                  Already have an account?
+                  <a href="#" className="ml-1 label-text-alt text-tertriary">
+                    Log in
+                  </a>
+                </label>
+              </label>
+            </div>
+            <button className="btn btn-block btn-info" type="submit">
+              Daftar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- fe-pwl/pages/signup.js
+++ fe-pwl/pages/signup.js
@@ -1,3 +1,14 @@
+import SignUp from '../components/SignUp';
+import SignIn from '../components/SignIn';
+import Image from 'next/image';
+import SignUpHero from '../public/svg/SignUpHero';
 export default function () {
-  return <div>Enter</div>;
+  return (
+    <div className="flex items-center justify-around min-h-screen bg-tertriary">
+      <SignUp></SignUp>
+      <div className="">
+        <SignUpHero width={500} height={500}></SignUpHero>
+      </div>
+    </div>
+  );
 }

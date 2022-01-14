7d4db4e66c7c6c66a987f69825c6d667e6e9b3ad
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-01-14 14:31:09 +0700

    fix: try fix icon size production

--- components/TeamList.js
+++ components/TeamList.js
@@ -5,7 +5,9 @@ const TeamList = ({ name, nim, work }) => {
     <div className="card shadow-2xl bg-gray-400 w-52 h-auto">
       <div className="card-body">
         <div className="justify-center card-actions">
-          <AccountCircleIcon className="w-24 h-24 text-blue-600" />
+          <div className="w-20 h-20">
+            <AccountCircleIcon className="w-full h-full text-blue-600" />
+          </div>
         </div>
         <div className="w-full flex flex-col text-black text-lg items-center pt-4 text-center gap-2">
           <div className="flex flex-col">
